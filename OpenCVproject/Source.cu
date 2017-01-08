88e76bc6aece523dde3999cf760b3881545c5fcb
remkruk <kruk.remigiusz@gmail.com>
2017-01-08 17:28:40 +0100

    probably ready

--- OpenCVproject/Source.cu
+++ OpenCVproject/Source.cu
@@ -17,22 +17,18 @@ using namespace cv;
 using namespace std;
 #include <string>
 
-#ifndef WINDOW_SIZE
 #define WINDOW_SIZE (5)
-#endif
-
+#define FILTER_SIZE (WINDOW_SIZE*WINDOW_SIZE)
 #define TILE_SIZE 16
+#define MEDIAN_INDEX (FILTER_SIZE/2 +1)
 
-__global__ void convolveKernel(unsigned const char* src, unsigned char* dist) {
-
-}
 
 __global__ void medianFilterKernel(unsigned char *inputImageKernel, unsigned char *outputImagekernel, int imageWidth, int imageHeight)
 {
 	// Set row and colum for thread.
 	int row = blockIdx.y * blockDim.y + threadIdx.y;
 	int col = blockIdx.x * blockDim.x + threadIdx.x;
-	unsigned char filterVector[9] = { 0,0,0,0,0,0,0,0,0 };   //Take fiter window
+	unsigned char filterVector[FILTER_SIZE];   //Take fiter window
 	if ((row == 0) || (col == 0) || (row == imageHeight - 1) || (col == imageWidth - 1))
 		outputImagekernel[row*imageWidth + col] = 0; //Deal with boundry conditions
 	else {
@@ -41,8 +37,8 @@ __global__ void medianFilterKernel(unsigned char *inputImageKernel, unsigned cha
 				filterVector[x*WINDOW_SIZE + y] = inputImageKernel[(row + x - 1)*imageWidth + (col + y - 1)];   // setup the filterign window.
 			}
 		}
-		for (int i = 0; i < 9; i++) {
-			for (int j = i + 1; j < 9; j++) {
+		for (int i = 0; i < FILTER_SIZE; i++) {
+			for (int j = i + 1; j < FILTER_SIZE; j++) {
 				if (filterVector[i] > filterVector[j]) {
 					//Swap the variables.
 					char tmp = filterVector[i];
@@ -51,7 +47,7 @@ __global__ void medianFilterKernel(unsigned char *inputImageKernel, unsigned cha
 				}
 			}
 		}
-		outputImagekernel[row*imageWidth + col] = filterVector[4];   //Set the output variables.
+		outputImagekernel[row*imageWidth + col] = filterVector[12];   //Set the output variables.
 	}
 }
 
@@ -67,12 +63,12 @@ unsigned char* createImageBuffer(unsigned int bytes, unsigned char **devicePtr)
 
 int main(int argc, char** argv)
 {
-	VideoCapture camera(0);
+	/*VideoCapture camera(0);
 	Mat frame;
 	if (!camera.isOpened())
 		return -1;
 
-	camera >> frame;
+	camera >> frame;*/
 	//IplImage* img = cvLoadImage("lena.jpg", 1);
 	//IplImage* dst = cvCreateImage(cvGetSize(img), IPL_DEPTH_8U, 3);
 	//double a[9] = { 1.0, 2.0, 1.0,
@@ -86,7 +82,7 @@ int main(int argc, char** argv)
 
 	//namedWindow("Before", CV_WINDOW_AUTOSIZE);
 
-	//Mat src = imread("lena.jpg", 1);
+	Mat src = imread("lenaSzum.jpeg", 1);
 
 	Mat dst;
 
@@ -98,23 +94,20 @@ int main(int argc, char** argv)
 	//imshow("Median filter", dst);
 
 
-	int width = frame.size().width;
-	int height = frame.size().height;
+	int width = src.size().width;
+	int height = src.size().height;
 
 	unsigned char *sourceDataDevice, *filteredDataDevice;
-	Mat source(frame.size(), CV_8U, createImageBuffer(width * height, &sourceDataDevice));
-	Mat filtered(frame.size(), CV_8U, createImageBuffer(width * height, &filteredDataDevice));
+	Mat source(src.size(), CV_8U, createImageBuffer(width * height, &sourceDataDevice));
+	Mat filtered(src.size(), CV_8U, createImageBuffer(width * height, &filteredDataDevice));
 
 	dim3 dimBlock(TILE_SIZE, TILE_SIZE);
 	dim3 dimGrid((int)ceil((float)width / (float)TILE_SIZE),
 		(int)ceil((float)height / (float)TILE_SIZE));
 
-	while (1)
-	{
-		camera >> frame;
-		cvtColor(frame, source, CV_BGR2GRAY);
+		cvtColor(src, source, CV_BGR2GRAY);
 		clock_t start = clock();
-		medianBlur(source, dst, 1);
+		medianBlur(source, dst, 3);
 		clock_t end = clock();
 		double time = double(end - start) / ((double)CLOCKS_PER_SEC / 1000);
 		cout << "Czas filtracji na CPU: " << time << "ms\n";
@@ -132,6 +125,5 @@ int main(int argc, char** argv)
 		imshow("source", source);
 		imshow("filtered", filtered);
 		imshow("filtered with opencv", dst);
-		if (cv::waitKey(1) == 27) break;
-	}
+		waitKey();
 }
